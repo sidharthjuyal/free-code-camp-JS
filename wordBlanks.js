59f64010364eaf517eecddb83ec9cbf0360b5f6d
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-28 10:49:28 +0000

    word blank

---
+++ wordBlanks.js
@@ -0,0 +1,8 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb){
+    let result = "";
+    result = myAdjective + myNoun + myVerb + myAdverb; 
+    return result;
+}
+
+console.log(wordBlanks("Sidharth ","Good ","coded ","quickly "));
+console.log(wordBlanks("Ayush ","Good ","marketed ","quickly "));
