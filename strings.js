e80c50e40f0a4344b06d14f623a30a49a3d283e0
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-28 15:34:41 +0530

    strings.js

---
+++ strings.js
@@ -0,0 +1,22 @@
+// string variable
+let string = `This is 'single quote' and this is "double quotes"`;
+console.log(string);
+
+// concatinate strings
+let firstName = "good coder ";
+let lastName = "Sidharth";
+let student = firstName + lastName;
+console.log(student);
+console.log("My name is " + lastName + " and my rank is 1");
+
+// length of string
+let length = firstName.length;
+console.log(length);
+
+// bracket notation
+let firstLetter = firstName[0];
+console.log(firstLetter);
+
+
+
+
