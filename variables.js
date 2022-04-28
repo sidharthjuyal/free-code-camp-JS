ed48c5ef194dd686dcb1f06eea4ac3ea3b5b43c9
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-28 09:47:41 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+/*  Data types provided by js  */
+//  undefined, null, boolean, string, symbol, number and object
+
+
+// variable : a memory location which holds data and have a name
+
+// var keyword
+// var can be used throughout your program
+var myName = "sidharth";
+myName = 8;
+
+// let keyword
+// only be used within the scope of it's declaration
+let ourName = "freecodecamp";
+
+// const keyword
+// value of const cannot be changed
+const pi = 3.14;
+
+// printing variables
+console.log(myName, ourName, pi);
+
+
+
