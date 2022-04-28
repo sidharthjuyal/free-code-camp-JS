624a4deabcae1f588d05be0c2b27f9ecd6980404
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-28 09:48:12 +0000

    basic maths

---
+++ basicMath.js
@@ -0,0 +1,46 @@
+// adding
+let sum = 10 + 10;
+console.log(sum);
+
+let line = "22 + 22";
+console.log(eval(line));
+
+// subtract
+let difference = 10 - 5;
+console.log(difference);
+
+// multiply
+let multiply = 10 * 5;
+console.log(multiply);
+
+// divide
+let divide = 10 / 5;
+console.log(divide);
+
+// increment
+let increment = 10;
+increment++;
+console.log(increment);
+
+// increment
+let decrement = 10;
+decrement--;
+console.log(decrement);
+
+// float
+let decimal = 5.7;
+
+// multiply decimal
+let multi = 2.0 * 2.5;
+console.log(multi);
+
+// divide decimal
+let div = 2.0 * 2.5;
+console.log(div);
+
+// remainder
+let remainder = 10 % 3;
+console.log(remainder);
+
+
+ 
