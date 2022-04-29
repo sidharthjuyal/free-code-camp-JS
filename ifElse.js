cf9c0157bf40c076adc009dae532ff83284dd8c9
Sidharth juyal <sid.juyal.34@gmail.com>
2022-04-29 18:08:41 +0000

    if - else

---
+++ ifElse.js
@@ -0,0 +1,11 @@
+function ourTrueOrFalse(isItTrue){
+    if(isItTrue) 
+      return "true It is!";
+    else
+      return "false It is";  
+}
+
+let a=1, b=5;
+let answer = ourTrueOrFalse(a>b);
+console.log(answer);
+
